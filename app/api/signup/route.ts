63d2df031a8a08167837f47dfa13985f2d6dbce5
rubenbernardes-rgb <ruben.bernardes@deselect.com>
2025-10-27 18:00:57 +0000

    first commit

---
+++ app/api/signup/route.ts
@@ -0,0 +1,14 @@
+import { start } from "workflow/api";
+import { NextResponse } from "next/server";
+import { handleUserSignup } from "../../workflows/user-signup";
+
+export async function POST(request: Request) {
+  const { email } = await request.json();
+
+  // Executes asynchronously and doesn't block your app
+  await start(handleUserSignup, [email]);
+
+  return NextResponse.json({
+    message: "User signup workflow started",
+  });
+}
