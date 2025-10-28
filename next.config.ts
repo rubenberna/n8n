a5218454a72187602e81ece600ae433f88839e25
rubenbernardes-rgb <ruben.bernardes@deselect.com>
2025-10-28 12:45:33 +0000

    database

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    cacheComponents: true,
+  },
 };
 
 export default withWorkflow(nextConfig);
